3ac59b8188607e8d029f3bd820d2765a60a2bd7f
Elena Novinkova <ryaza_nova@inbox.ru>
2022-05-18 12:55:23 +0300

    Написание кода для решения задач

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,53 @@
+'use strict';
+
+// Задача 1
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    return arr;
+  } else if (d == 0) {
+    arr.push(-b / 2 * a);
+    return arr;
+  } else if (d < 0) {
+    return arr;
+  } 
+} 
+
+// Задача 2
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+
   let totalAmount;
+  let d = new Date();
+  percent = percent / 100;
+  let P = percent / 12;
+  let loanPrincipal = amount - contribution;
+  let months = Math.round((date - d) / 2548800000);
+  let monthlyPayment;
 
-  // код для задачи №2 писать здесь
+  if ((percent <= 0) || (isNaN(percent))) {
+    return 'Параметр "Процентная ставка" содержит неправильное значение "test"';
+  } 
+  if ((contribution < 0) || (isNaN(contribution))) {
+    return 'Параметр "Начальный взнос" содержит неправильное значение "test"';
+  } 
+  if ((amount <= 0) || (isNaN(amount))) {
+    return 'Параметр "Общая стоимость" содержит неправильное значение "test"';
+  } 
 
-  return totalAmount;
+  if (date <= d) {
+    return 'Срок ипотеки не может быть меньше или равен текущей даты!';
+  } else if (isNaN(date)) {
+    return 'Введите дату в поле Срок ипотеки!';
+  }
+
+  monthlyPayment = loanPrincipal * (P + (P / (((1 + P)**months) - 1)));
+  totalAmount = monthlyPayment * months;
+
+  return Number(totalAmount.toFixed(2));
 }
