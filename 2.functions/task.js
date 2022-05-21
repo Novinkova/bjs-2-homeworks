994b2a3f576100ce10e810783d818a348f36a05f
Elena Novinkova <ryaza_nova@inbox.ru>
2022-05-21 21:23:40 +0300

    Homework done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,56 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
 
-  return { min: min, max: max, avg: avg };
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
+
+  return {min: min, max: max, avg: avg}; 
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  let sum = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i ++) {
+    sum = func(arrOfArr[i]);
+
+    if (sum > max) {
+      max = sum;
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let params = getArrayParams(arr);
+  let difference = Math.abs(params.max - params.min);
+
+  return difference;
 }
