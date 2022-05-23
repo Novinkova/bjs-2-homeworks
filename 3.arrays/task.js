a8749fbca6184b5937a1853fa52c1e2cecda408f
Elena Novinkova <ryaza_nova@inbox.ru>
2022-05-23 14:22:42 +0300

    Homework done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.every((number, idx) => number === arr2[idx]) && arr1.length === arr2.length;
+  
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((number) => number >= 0).filter((number) => number % 3 == 0).map((number) => number * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr; 
 }
