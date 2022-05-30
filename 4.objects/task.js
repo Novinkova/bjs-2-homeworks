f095bc6e11a1ae64ee44e11116789e972dcfe7f5
Elena Novinkova <ryaza_nova@inbox.ru>
2022-05-30 18:33:36 +0300

    Homework done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,48 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+const Student1 = new Student("Николай", "мужской", "20");
+const Student2 = new Student("Елена", "женский", "23");
+const Student3 = new Student("Мария", "женский", "19"); 
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+     this.marks = [];
+  } 
+
+    this.marks.push(mark);
+} 
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) { 
+    this.marks = [];
+  } 
+
+   marks.forEach(mark => this.marks.push(mark));
+} 
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let devisor = this.marks.length;
+
+  if (this.marks === undefined) {
+    return 'Оценок нет';
+  } else {
+    this.marks.forEach(mark => sum += mark); 
+  }
+  return sum / devisor;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  
+  this.excluded = reason;
+}
